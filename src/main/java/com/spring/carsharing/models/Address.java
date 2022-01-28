b2232e0f59a180873083162eaec9bd561efdfc79
Bilal Abz <billabz23@gmail.com>
2022-01-28 17:32:16 +0100

    address class

--- src/main/java/com/spring/carsharing/models/Address.java
+++ src/main/java/com/spring/carsharing/models/Address.java
@@ -1,4 +1,53 @@
 package com.spring.carsharing.models;
 
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
+
+@Embeddable
 public class Address {
+    private String country;
+    private String street;
+    private String city;
+    private String state;
+    private String postCode;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
 }
