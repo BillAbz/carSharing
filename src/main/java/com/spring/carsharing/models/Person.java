e7f5547d612def69438ee4292bd0fb945f40a1f9
Bilal Abz <billabz23@gmail.com>
2022-01-28 17:32:53 +0100

    person class

--- src/main/java/com/spring/carsharing/models/Person.java
+++ src/main/java/com/spring/carsharing/models/Person.java
@@ -1,4 +1,88 @@
 package com.spring.carsharing.models;
 
+import org.springframework.validation.annotation.Validated;
+
+import javax.persistence.*;
+
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue
+    private Integer personId;
+    private String name;
+    private String idNumber;
+    private String phoneNumber;
+    private String email;
+    @Embedded
+    private Address address;
+    @OneToOne
+    @JoinColumn(name = "account_person_id")
+    private Account account;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "personId=" + personId +
+                ", name='" + name + '\'' +
+                ", idNumber='" + idNumber + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public Integer getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Integer personId) {
+        this.personId = personId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
 }
